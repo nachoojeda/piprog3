e9fbc64c420ce1088e9156729e5e95b5610397fa
Nacho J Ojeda Vecellio <nachojojedavecellio@Nachimac.fibertel.com.ar>
2022-09-08 10:36:26 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-/*import { Route, Switch } from "react-router-dom";*/
+/*import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from '../src/screens/Home/Home'
@@ -24,12 +24,36 @@ function App() {
   );
 }
 
-export default App;
-
- 
-
-
+export default App; */
 
    /*     <Route path = '/TodasPeliculas' component = {TodasPeliculas} /> */
 
+   import React from "react";
+   import {Route, Switch} from "react-router-dom";
+   import Footer from "./components/Footer/Footer";
+   import Header from "./components/Header/Header";
+   import List from './components/List/List'
+    import Home from './screens/Home/Home'
+  
+   
+   
+   function App() {
+       
+   return(
+       <React.Fragment>
+           
+           <Header/>
+   
+           <Switch>
+   
+           <Route path = '/' exact = {true} component = {Home}/>
+  
+           </Switch>
+           
+           <Footer/>
+       </React.Fragment>
+   );
+   }
+   
+   export default App;
       

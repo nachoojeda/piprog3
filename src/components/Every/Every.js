02b413f6648b7c97c18009d3b8f1f8a7fc9624c2
Tomas merlo <tommymerlo99@gmail.com>
2022-09-12 12:17:51 -0300

    ver todas de canciones hecho, falta albumes

--- src/components/Every/Every.js
+++ src/components/Every/Every.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react'
+import Card from '../Card/Card'
 class Every extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      charts: [],
+      backup: [],
+    //   pagina:0,
+      favorito:[]
+    }
+  }
+  componentDidMount(){
+    fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks?index=12&limit=32')
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      this.setState({
+      charts: data.data,
+      backup: data.data,
+    //   pagina: this.state.pagina + 1
+    })})
+    .catch(err => console.log(err))
+  }
   render() {
     return (
-      <div>Every</div>
+      <>
+      <h1 className='titulo'>Todas las canciones y albumes</h1>
+      <section className="card-container">
+        {this.state.charts.map((chart, idx) => 
+          <Card 
+            key={`${Date.now()}-${idx}`}  
+            info={chart}
+            borrar={(name)=> this.borrar(name)}
+            favorito={(id)=> this.favorites(id)}
+          />)}
+
+          <button onClick={()=> this.backup()}>Volver atras</button> 
+      </section>
+    </>
     )
   }
 }
